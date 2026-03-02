9697042fed5607dd2bf17207a911fe1136e36009
BakilTechh <mhmmdbakilamru31@gmail.com>
2026-03-02 23:35:35 +0800

    Final

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -22,36 +22,31 @@ int main() {
 }
 
 /*
-Analisis Kompleksitas Waktu:
+    Analisis Kompleksitas Waktu Algoritma cariMinIndex:
     ---------------------------------------------------------------
-    | Baris | Pseudocode              | Cost        | Tmin | Tmax |
+    | Baris | Pseudocode             | Label | Cost | Tmin | Tmax |
     ---------------------------------------------------------------
-    |   1   | min ← arr[0]            | 1           |  1   |  1   |
-    |   2   | for i ← 1 to n-1        | (n-1)       | n-1  | n-1  |
-    |   3   | if arr[i] < min         | (n-1)       | n-1  | n-1  |
-    |   4   | min ← arr[i]            | 0..(n-1)    |  0   | n-1  |
-    |   5   | return min              | 1           |  1   |  1   |
+    |   1   | procedure FindMin      |  C1   |  1   |  1   |  1   |
+    |   2   | min ← A[0]             |  C2   |  1   |  1   |  1   |
+    |   3   | for i ← 1 to n − 1     |  C3   |  n   |  n   |  n   |
+    |   4   | if A[i] < min          |  C4   | n-1  | n-1  | n-1  |
+    |   5   | min ← A[i]             |  C5   | 0..n-1 | 0  | n-1 |
+    |   6   | end if                 |  C6   | n-1  | n-1  | n-1  |
+    |   7   | end for                |  C7   |  n   |  n   |  n   |
+    |   8   | return min             |  C8   |  1   |  1   |  1   |
+    |   9   | end procedure          |  C9   |  1   |  1   |  1   |
     ---------------------------------------------------------------
 
-[------------------------------------------------------------------]
-Penjelasan :
-Baris 1: 1 kali → cost = 1
-Baris 2: (n-1) kali → cost = n-1
-Baris 3: (n-1) kali → cost = n-1
-Baris 4: Best case = 0 kali, Worst case = n-1 kali
-Baris 5: 1 kali → cost = 1
+    [-----------------------------------------------------------------]
+    
+    Perhitungan:
+    Tmin(n) = 1 + 1 + n + (n-1) + 0 + (n-1) + n + 1 + 1
+            = 3n + 3 → O(n)
 
-Tmin(n) :
-= 1 + (n-1) + (n-1) + 0 + 1
-= 2n - 2 + 2
-= 2n
-→ O(n)
+    Tmax(n) = 1 + 1 + n + (n-1) + (n-1) + (n-1) + n + 1 + 1
+            = 4n + 3 → O(n)
 
-Tmax(n) : 
-1 + (n-1) + (n-1) + (n-1) + 1
-= 3n - 3 + 2
-= 3n - 1
-→ O(n)
+
 
 Kesimpulannya:
 - Tmin(n) = 2n → O(n)
